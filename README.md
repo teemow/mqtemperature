596778624221351a0138e834d361e863b8bd0d49
Timo Derstappen <teemow@gmail.com>
2016-08-23 00:23:40 +0200

    add config file and handle multiple devices

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # MQTT Temperature (DS18S20)
 
 Raspberry Pi One-Wire temperature publisher
+
+## Build for raspberry pi
+
+```
+make
+```
+
+## Configuration
+
+Default: `/etc/mqtemperature/config.yaml`
+
+```
+devices:
+    "28-03146c2288ff": "hotwater"
+    "28-03146cd21fff": "hotwater-return"
+diffs:
+    - topic: "hotwater-diff"
+      device1: "28-03146c2288ff"
+      device2: "28-03146cd21fff"
+```
+
+## Run
+
+```
+mqtemperature  --host=my-mqtt.server --interval=20
+```
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	mqtt "git.eclipse.org/gitroot/paho/org.eclipse.paho.mqtt.golang.git"
+	"github.com/pborman/uuid"
 	"github.com/spf13/cobra"
 	"github.com/teemow/mqtemperature/onewire"
 )
@@ -22,8 +23,7 @@ var (
 	mainFlags struct {
 		Host     string
 		Port     int
-		Topic    string
-		Device   string
+		Config   string
 		Interval int
 	}
 
@@ -43,8 +43,7 @@ func init() {
 	mainCmd.PersistentFlags().BoolVarP(&globalFlags.verbose, "verbose", "v", false, "Print verbose output")
 	mainCmd.PersistentFlags().StringVar(&mainFlags.Host, "host", "localhost", "MQTT host")
 	mainCmd.PersistentFlags().IntVar(&mainFlags.Port, "port", 1883, "MQTT port")
-	mainCmd.PersistentFlags().StringVar(&mainFlags.Topic, "topic", "", "MQTT topic")
-	mainCmd.PersistentFlags().StringVar(&mainFlags.Device, "device", "", "DS18S20 device")
+	mainCmd.PersistentFlags().StringVar(&mainFlags.Config, "config", "/etc/mqtemperature/config.yml", "Configuration file")
 	mainCmd.PersistentFlags().IntVar(&mainFlags.Interval, "interval", 60, "Interval in seconds")
 }
 
@@ -59,39 +58,76 @@ func assert(err error) {
 	}
 }
 
-func watchTemperature(client *mqtt.Client, device *onewire.DS18S20) {
-	for {
-		value, err := device.Read()
-		if err == nil {
-			topic := fmt.Sprintf("mqtemperature/%s", mainFlags.Topic)
-			payload := fmt.Sprintf("%d", value)
+func watchTemperature(client *mqtt.Client) {
+	conf, err := loadConfig(mainFlags.Config)
+	if err != nil {
+		log.Fatalf("Couldn't read config: %s", err)
+	}
 
-			if token := client.Publish(topic, 1, false, payload); token.WaitTimeout(500) && token.Error() != nil {
-				fmt.Printf("Failed to send message: %v\n", token.Error())
-			}
-		} else {
-			fmt.Println("Could not read temperature")
+	for {
+		temperatures, err := readTemperatures(conf.Devices)
+		if err != nil {
+			log.Printf("Failed to read temperatures: %v\n", err)
 		}
+
+		for d, topic := range conf.Devices {
+			err := sendMsg(client, topic, temperatures[d])
+
+			if err != nil {
+				log.Printf("Failed to send message: %v\n", err)
+			}
+		}
+
+		for _, diff := range conf.Diffs {
+			err := sendMsg(client, diff.Topic, temperatures[diff.Device1]-temperatures[diff.Device2])
+
+			if err != nil {
+				log.Printf("Failed to send message: %v\n", err)
+			}
+		}
+
 		time.Sleep(time.Duration(mainFlags.Interval) * time.Second)
 	}
+}
+
+func sendMsg(client *mqtt.Client, topic string, temperature int64) error {
+	topic = fmt.Sprintf("mqtemperature/%s", topic)
+	payload := fmt.Sprintf("%d", temperature)
+
+	token := client.Publish(topic, 1, false, payload)
+
+	token.WaitTimeout(500)
+	return token.Error()
+}
+
+func readTemperatures(devices map[string]string) (map[string]int64, error) {
+	temperatures := map[string]int64{}
+
+	for d := range devices {
+		device, err := onewire.NewDS18S20(d)
+		if err != nil {
+			log.Printf("Device %s not found\n", d)
+		}
+
+		temperatures[d], err = device.Read()
+		if err != nil {
+			log.Printf("Couldn't read value of %s\n", d)
+		}
+	}
+	return temperatures, nil
 }
 
 func mainRun(cmd *cobra.Command, args []string) {
 	// mqtt
 	opts := mqtt.NewClientOptions().AddBroker(fmt.Sprintf("tcp://%s:%d", mainFlags.Host, mainFlags.Port))
-	opts.SetClientID(fmt.Sprintf("mqtemperature-%s", mainFlags.Topic))
+	opts.SetClientID(fmt.Sprintf("mqtemperature-%s", uuid.New()))
 
 	client := mqtt.NewClient(opts)
 	if token := client.Connect(); token.Wait() && token.Error() != nil {
 		log.Println(token.Error())
 	}
 
-	device, err := onewire.NewDS18S20(mainFlags.Device)
-	if err != nil {
-		log.Fatalf("Device %s not found", mainFlags.Device)
-	}
-
-	go watchTemperature(client, device)
+	go watchTemperature(client)
 
 	// Handle SIGINT and SIGTERM.
 	ch := make(chan os.Signal)
@@ -103,7 +139,6 @@ func mainRun(cmd *cobra.Command, args []string) {
 
 func main() {
 	mainCmd.AddCommand(versionCmd)
-	mainCmd.AddCommand(diffCmd)
 
 	mainCmd.Execute()
 }
